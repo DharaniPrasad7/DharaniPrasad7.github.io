7670a41f27e91bab5a9465996f30683c4e5e0aad
Dharani Prasad <55753231+DharaniPrasad7@users.noreply.github.com>
2023-09-29 13:50:08 +0530

    Update main_jquery.js

--- main_jquery.js
+++ main_jquery.js
@@ -1,4 +1,8 @@
 /*! jQuery v3.5.1 | (c) JS Foundation and other contributors | jquery.org/license */
+$( '.menu-outer-wrapper' ).click(function(){
+    $('.div-block-79').slideToggle('slow');
+});
+
 !function(e, t) {
     "use strict";
     "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
